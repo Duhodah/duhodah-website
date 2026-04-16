a7f99e29d291940b0dc271e0d4d5c02d0e0fa800
Duhodah <duhodahos@gmail.com>
2026-04-16 11:39:21 +0200

    heh

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -7,4 +7,10 @@ export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiO
 
 // Supabase JS client (v2, ESM module)
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    flowType: 'pkce',
+    detectSessionInUrl: true,
+    persistSession: true,
+  }
+});
